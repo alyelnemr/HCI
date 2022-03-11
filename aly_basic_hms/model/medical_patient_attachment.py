985add96dba7000aefa5e3a6f1161b6496237a16
Aly Abdel Hamid <aly5elnemr@gmail.com>
2022-03-11 20:48:24 +0200

    fix patient attachment screen

--- aly_basic_hms/model/medical_patient_attachment.py
+++ aly_basic_hms/model/medical_patient_attachment.py
@@ -7,5 +7,5 @@ class MedicalPatientAttachment(models.Model):
 
     name = fields.Char(string='File Name', required=True)
     patient_id = fields.Many2one('medical.patient', 'Patient ID')
-    # att_document = fields.Binary(string='Attachment', required=True)
+    att_document = fields.Binary(string='Attachment', required=True)
     short_comment = fields.Char('Comment', size=128)
