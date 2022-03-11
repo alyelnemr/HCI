dd41d405e6fa65b4380f1e2cad124b074de3aa58
Aly Abdel Hamid <aly5elnemr@gmail.com>
2022-03-11 21:04:33 +0200

    fix patient attachment screen

--- aly_basic_hms/model/medical_patient_attachment.py
+++ aly_basic_hms/model/medical_patient_attachment.py
@@ -5,10 +5,7 @@ class MedicalPatientAttachment(models.Model):
     _name = 'medical.patient.attachment'
     _description = 'description'
 
-    name = fields.Many2one('medical.patient', 'Patient ID')
+    name = fields.Char(string='File Name', required=True)
     patient_id = fields.Many2one('medical.patient', 'Patient ID')
-    product_id = fields.Many2one('product.product', 'Disposable', domain=[('sale_ok', '=', 1), ('type', '=', 'consu')],
-                                 required=True)
-    quantity = fields.Integer('Quantity', default=1)
+    att_document = fields.Binary(string='Attachment', required=True)
     short_comment = fields.Char('Comment', size=128)
-    company_id = fields.Many2one('res.company', required=True, readonly=False, default=lambda self: self.env.user.company_id)
