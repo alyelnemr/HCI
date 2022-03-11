6d72d21386c600d516d63943c5feb3e6f4486bf2
Aly Abdel Hamid <aly5elnemr@gmail.com>
2022-03-11 17:55:31 +0200

    fix time out for operation

--- aly_basic_hms/model/__init__.py
+++ aly_basic_hms/model/__init__.py
@@ -6,6 +6,7 @@ from . import medical_patient_evaluation
 from . import medical_diet_belief
 from . import medical_diet_therapeutic
 from . import medical_appointment
+from . import medical_appointment_line
 from . import medical_appointment_consultation_line
 from . import medical_appointment_procedure
 from . import medical_appointment_investigation
