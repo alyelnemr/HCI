bcfedbb47564cfed40d55ffd2ff9f2f0b8bd9a30
Aly Abdel Hamid <aly5elnemr@gmail.com>
2022-03-11 20:41:03 +0200

    fix patient attachment screen

--- aly_basic_hms/model/medical_patient_attachment.py
+++ aly_basic_hms/model/medical_patient_attachment.py
@@ -1,11 +1,11 @@
 from odoo import api, fields, models, _
 
 
-class MedicalPatientAttachment(models.Model):
+class MedicalPatientLine(models.Model):
     _name = 'medical.patient.attachment'
     _description = 'description'
 
     name = fields.Char(string='File Name', required=True)
-    patient_id = fields.Many2one('medical.patient')
-    att_document = fields.Binary(string='Attachment', required=True)
+    patient_id = fields.Many2one('medical.patient', 'Patient ID')
+    # att_document = fields.Binary(string='Attachment', required=True)
     short_comment = fields.Char('Comment', size=128)
