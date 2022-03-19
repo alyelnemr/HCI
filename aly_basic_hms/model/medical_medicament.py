c9a8a3ca494130210f1aaafb4ab01f6a2bb48803
Aly Abdel Hamid <aly5elnemr@gmail.com>
2022-03-19 14:05:05 +0200

    fix medicines filtration

--- aly_basic_hms/model/medical_medicament.py
+++ aly_basic_hms/model/medical_medicament.py
@@ -19,7 +19,18 @@ class medical_medicament(models.Model):
                 self.qty_available = 0
                 self.price = 0.0
 
-    product_id = fields.Many2one('product.product', 'Name')
+    def _get_medicine_product_category_domain(self):
+        accom_prod_cat = self.env['ir.config_parameter'].sudo().get_param('medicine.product_category')
+        prod_cat_obj = self.env['product.category'].search([('name', '=', accom_prod_cat)])
+        prod_cat_obj_id = 0
+        if len(prod_cat_obj) > 1:
+            prod_cat_obj_id = prod_cat_obj[0].id
+        else:
+            prod_cat_obj_id = prod_cat_obj.id
+        return [('categ_id', '=', prod_cat_obj_id), ('sale_ok', '=', 1), ('type', '=', 'product')]
+
+    product_id = fields.Many2one('product.product', string='Name',
+                                 domain=lambda self: self._get_medicine_product_category_domain(), required=True)
     therapeutic_action = fields.Char('Therapeutic effect', help = 'Therapeutic action')
     price = fields.Float(compute=onchange_product,string='Price',store=True)
     qty_available = fields.Integer(compute=onchange_product,string='Quantity Available',store=True)
@@ -27,19 +38,6 @@ class medical_medicament(models.Model):
     active_component = fields.Char(string="Active Component")
     presentation = fields.Text('Presentation')
     composition = fields.Text('Composition')
-    dosage = fields.Text('Dosage Instructions')
-    pregnancy = fields.Text('Pregnancy')
-    overdosage = fields.Text('Overdosage')
-    pregnancy_warning = fields.Boolean('Pregnancy Warning')
-    pregnancy_category = fields.Selection([('a','A'),('b','B'), ('c','C'), ('d', 'D'), ('x', 'X'), ('n','N')], help = """"** FDA Pregancy Categories ***CATEGORY A :Adequate and well-controlled human studies have failed to demonstrate a risk to the fetus in the first trimester of pregnancy (and there is no evidence of risk in later trimesters)CATEGORY B : Animal reproduction studies have failed todemonstrate a risk to the fetus and there are no adequate and well-controlled studies in pregnant women OR Animal studies have shown an adverse effect, but adequate and well-controlled studies in pregnant women have failed to demonstrate a risk to the fetus in any trimester.
-
-CATEGORY C : Animal reproduction studies have shown an adverse effect on the fetus and there are no adequate and well-controlled studies in humans, but potential benefits may warrant use of the drug in pregnant women despite potential risks. 
-
- CATEGORY D : There is positive evidence of human fetal  risk based on adverse reaction data from investigational or marketing experience or studies in humans, but potential benefits may warrant use of the drug in pregnant women despite potential risks.
-
-CATEGORY X : Studies in animals or humans have demonstrated fetal abnormalities and/or there is positive evidence of human fetal risk based on adverse reaction data from investigational or marketing experience, and the risks involved in use of the drug in pregnant women clearly outweigh potential benefits.
-
-CATEGORY N : Not yet classified""")
 
     adverse_reaction = fields.Text('Adverse Reactions')
     storage = fields.Text('Storage Condition')
