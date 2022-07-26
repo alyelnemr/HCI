36f66f0356abbc24590b32a7655db6acd857b28a
Aly Abdel Hamid <aly5elnemr@gmail.com>
2022-07-26 02:40:36 +0200

    [fix] date time zone issue for all reports

--- aly_basic_hms/model/MedicalReportTemplate.py
+++ aly_basic_hms/model/MedicalReportTemplate.py
@@ -67,13 +67,17 @@ class MedicalReportTemplate(models.AbstractModel):
         user_tz = self.env.user.tz or pytz.utc
 
         local = pytz.timezone(user_tz)
-        if docs.update_note_ids:
-            sorted_update_note = sorted(docs.update_note_ids, key=lambda a: a.appointment_date)
-            min_date = pytz.utc.localize(sorted_update_note[0].appointment_date).astimezone(local).strftime("%d/%m/%Y %H:%M:%S")
-        elif docs.inpatient_ids:
-            sorted_update_note = sorted(docs.inpatient_ids, key=lambda a: a.admission_date)
-            # min_date = pytz.utc.localize(sorted_update_note[0].admission_date).astimezone(local).strftime("%d/%m/%Y %H:%M:%S")
-            min_date = sorted_update_note[0].admission_date.strftime("%d/%m/%Y %H:%M:%S")
+        min_date_str = ''
+        if sorted_data:
+            if sorted_data[0].obj_type == 'app':
+                min_date_str = sorted_data[0].obj_id.appointment_date
+            if sorted_data[0].obj_type == 'inp':
+                min_date_str = sorted_data[0].obj_id.admission_date
+            if sorted_data[0].obj_type == 'inp_up':
+                min_date_str = sorted_data[0].obj_id.update_note_date
+            if sorted_data[0].obj_type == 'op':
+                min_date_str = sorted_data[0].obj_id.time_in
+        min_date = pytz.utc.localize(min_date_str).astimezone(local).strftime("%d/%m/%Y %H:%M:%S")
         var_room_number = str(docs.room_number)
         today_now = datetime.now()
         min_update_note_date = min_date if min_date else today_now.strftime("%d/%m/%Y %H:%M:%S")
@@ -89,7 +93,7 @@ class MedicalReportTemplate(models.AbstractModel):
             'min_update_note_date': min_update_note_date,
             'is_discharged': is_discharged,
             'discharge_datetime': discharge_datetime,
-            'report_title': 'Primary Medical Report'
+            'report_title': 'Final Medical Report'
         }
 
 
@@ -103,7 +107,7 @@ class MedicalReportTemplateUpdate(models.AbstractModel):
         for app in record.update_note_ids:
             my_list.append(
                 {
-                    'date': app.appointment_date.date(),
+                    'date': app.appointment_date,
                     'obj_type': 'app',
                     'order_in_list': i,
                     'obj_id': app
@@ -112,9 +116,10 @@ class MedicalReportTemplateUpdate(models.AbstractModel):
         i = 0
         # inpatient_update_note_ids
         for app in record.inpatient_ids:
+            admission_date = datetime.combine(app.admission_date, datetime.min.time())
             my_list.append(
                 {
-                    'date': app.admission_date,
+                    'date': admission_date,
                     'obj_type': 'inp',
                     'order_in_list': i,
                     'obj_id': app
@@ -123,7 +128,7 @@ class MedicalReportTemplateUpdate(models.AbstractModel):
             for inp in app.inpatient_update_note_ids:
                 my_list.append(
                     {
-                        'date': inp.update_note_date.date(),
+                        'date': inp.update_note_date,
                         'obj_type': 'inp_up',
                         'order_in_list': i,
                         'obj_id': inp
@@ -134,7 +139,7 @@ class MedicalReportTemplateUpdate(models.AbstractModel):
         for app in record.operation_ids:
             my_list.append(
                 {
-                    'date': app.time_in.date(),
+                    'date': app.time_in,
                     'obj_type': 'op',
                     'order_in_list': i,
                     'obj_id': app
@@ -151,12 +156,25 @@ class MedicalReportTemplateUpdate(models.AbstractModel):
         active_id = self.env.context.get('active_id')
         docs = self.env[model].sudo().browse(docids)
         sorted_data = self.get_sorting(docs)
-        sorted_update_note = sorted(docs.update_note_ids, key=lambda a: a.appointment_date)
+        user_tz = self.env.user.tz or pytz.utc
+
+        local = pytz.timezone(user_tz)
+        min_date_str = ''
+        if sorted_data:
+            if sorted_data[0].obj_type == 'app':
+                min_date_str = sorted_data[0].obj_id.appointment_date
+            if sorted_data[0].obj_type == 'inp':
+                min_date_str = sorted_data[0].obj_id.admission_date
+            if sorted_data[0].obj_type == 'inp_up':
+                min_date_str = sorted_data[0].obj_id.update_note_date
+            if sorted_data[0].obj_type == 'op':
+                min_date_str = sorted_data[0].obj_id.time_in
+        min_date = pytz.utc.localize(min_date_str).astimezone(local).strftime("%d/%m/%Y %H:%M:%S")
         var_room_number = str(docs.room_number)
         today_now = datetime.now()
-        min_update_note_date = sorted_update_note[0].appointment_date.strftime("%d/%m/%Y %H:%M:%S") \
-            if len(sorted_update_note) > 0 else today_now.strftime("%d/%m/%Y %H:%M:%S")
+        min_update_note_date = min_date if min_date else today_now.strftime("%d/%m/%Y %H:%M:%S")
         is_discharged = docs.inpatient_ids[0].is_discharged if len(docs.inpatient_ids) > 0 else False
+        discharge_datetime = pytz.utc.localize(docs.inpatient_ids[0].discharge_datetime).astimezone(local).strftime("%d/%m/%Y %H:%M:%S") if len(docs.inpatient_ids) > 0 and is_discharged else False
         return {
             'data': data,
             'doc_ids': docids,
@@ -166,6 +184,7 @@ class MedicalReportTemplateUpdate(models.AbstractModel):
             'var_room_number': var_room_number,
             'min_update_note_date': min_update_note_date,
             'is_discharged': is_discharged,
+            'discharge_datetime': discharge_datetime,
             'report_title': 'Update Medical Report'
         }
 
@@ -180,7 +199,7 @@ class MedicalReportTemplatePrimary(models.AbstractModel):
         for app in record.update_note_ids:
             my_list.append(
                 {
-                    'date': app.appointment_date.date(),
+                    'date': app.appointment_date,
                     'obj_type': 'app',
                     'order_in_list': i,
                     'obj_id': app
@@ -189,9 +208,10 @@ class MedicalReportTemplatePrimary(models.AbstractModel):
         i = 0
         # inpatient_update_note_ids
         for app in record.inpatient_ids:
+            admission_date = datetime.combine(app.admission_date, datetime.min.time())
             my_list.append(
                 {
-                    'date': app.admission_date,
+                    'date': admission_date,
                     'obj_type': 'inp',
                     'order_in_list': i,
                     'obj_id': app
@@ -200,7 +220,7 @@ class MedicalReportTemplatePrimary(models.AbstractModel):
             for inp in app.inpatient_update_note_ids:
                 my_list.append(
                     {
-                        'date': inp.update_note_date.date(),
+                        'date': inp.update_note_date,
                         'obj_type': 'inp_up',
                         'order_in_list': i,
                         'obj_id': inp
@@ -211,7 +231,7 @@ class MedicalReportTemplatePrimary(models.AbstractModel):
         for app in record.operation_ids:
             my_list.append(
                 {
-                    'date': app.time_in.date(),
+                    'date': app.time_in,
                     'obj_type': 'op',
                     'order_in_list': i,
                     'obj_id': app
@@ -228,12 +248,25 @@ class MedicalReportTemplatePrimary(models.AbstractModel):
         active_id = self.env.context.get('active_id')
         docs = self.env[model].sudo().browse(docids)
         sorted_data = self.get_sorting(docs)
-        sorted_update_note = sorted(docs.update_note_ids, key=lambda a: a.appointment_date)
+        user_tz = self.env.user.tz or pytz.utc
+
+        local = pytz.timezone(user_tz)
+        min_date_str = ''
+        if sorted_data:
+            if sorted_data[0].obj_type == 'app':
+                min_date_str = sorted_data[0].obj_id.appointment_date
+            if sorted_data[0].obj_type == 'inp':
+                min_date_str = sorted_data[0].obj_id.admission_date
+            if sorted_data[0].obj_type == 'inp_up':
+                min_date_str = sorted_data[0].obj_id.update_note_date
+            if sorted_data[0].obj_type == 'op':
+                min_date_str = sorted_data[0].obj_id.time_in
+        min_date = pytz.utc.localize(min_date_str).astimezone(local).strftime("%d/%m/%Y %H:%M:%S")
         var_room_number = str(docs.room_number)
         today_now = datetime.now()
-        min_update_note_date = sorted_update_note[0].appointment_date.strftime("%d/%m/%Y %H:%M:%S") \
-            if len(sorted_update_note) > 0 else today_now.strftime("%d/%m/%Y %H:%M:%S")
+        min_update_note_date = min_date if min_date else today_now.strftime("%d/%m/%Y %H:%M:%S")
         is_discharged = docs.inpatient_ids[0].is_discharged if len(docs.inpatient_ids) > 0 else False
+        discharge_datetime = pytz.utc.localize(docs.inpatient_ids[0].discharge_datetime).astimezone(local).strftime("%d/%m/%Y %H:%M:%S") if len(docs.inpatient_ids) > 0 and is_discharged else False
         return {
             'data': data,
             'doc_ids': docids,
@@ -243,5 +276,6 @@ class MedicalReportTemplatePrimary(models.AbstractModel):
             'var_room_number': var_room_number,
             'min_update_note_date': min_update_note_date,
             'is_discharged': is_discharged,
+            'discharge_datetime': discharge_datetime,
             'report_title': 'Primary Medical Report'
         }
