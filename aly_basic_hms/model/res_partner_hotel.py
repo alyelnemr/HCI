89e2b3b05d9b5d9d532472b6a9845fa7f564bc56
Aly Abdel Hamid <aly5elnemr@gmail.com>
2022-09-25 01:50:16 +0200

    [add] hotels occupancy rate3

---
+++ aly_basic_hms/model/res_partner_hotel.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models, _
+
+class res_partner(models.Model):
+    _inherit = 'res.partner.hotel'
+
+    hotel_id = fields.Many2one('res.partner', string='Hotel')
+    occupancy_rate = fields.Integer('Occupancy Rate')
+    occupancy_rate_date = fields.Date('Date', required=True, default=fields.Datetime.now)
