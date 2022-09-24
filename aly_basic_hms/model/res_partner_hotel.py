9279dd34fba12cc369d51778f9e989630e9fb968
Aly Abdel Hamid <aly5elnemr@gmail.com>
2022-09-25 01:53:20 +0200

    [add] hotels occupancy rate4

--- aly_basic_hms/model/res_partner_hotel.py
+++ aly_basic_hms/model/res_partner_hotel.py
@@ -4,7 +4,7 @@
 from odoo import api, fields, models, _
 
 class res_partner(models.Model):
-    _inherit = 'res.partner.hotel'
+    _name = 'res.partner.hotel'
 
     hotel_id = fields.Many2one('res.partner', string='Hotel')
     occupancy_rate = fields.Integer('Occupancy Rate')
