4ba647a651963292b89a2531997b698f793bcb58
Aly Abdel Hamid <aly5elnemr@gmail.com>
2022-10-26 00:49:19 +0200

    [fix] prevent create products for non managers

--- aly_basic_hms/model/__init__.py
+++ aly_basic_hms/model/__init__.py
@@ -9,6 +9,7 @@ from . import account_payment_register
 from . import sale_order
 from . import sale_order_payment
 from . import product_category
+from . import product_template
 from . import bed_transfer
 from . import medical_clinic
 from . import medical_appointment
