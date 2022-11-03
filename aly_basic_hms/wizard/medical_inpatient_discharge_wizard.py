0252903215d57a9664bf69b38944e45d3d900fd8
Aly Abdel Hamid <aly5elnemr@gmail.com>
2022-11-03 23:01:13 +0200

    [fix] clear medication when discharged before

--- aly_basic_hms/wizard/medical_inpatient_discharge_wizard.py
+++ aly_basic_hms/wizard/medical_inpatient_discharge_wizard.py
@@ -55,7 +55,7 @@ class MedicalInpatientInvoiceWizard(models.TransientModel):
             appointment_obj.admission_days = self.actual_admission_days
             # appointment_obj.discharge_medication_ids = self.discharge_medication_ids
             discharge_medication_ids = []
-            appointment_obj.discharge_medication_ids = discharge_medication_ids
+            appointment_obj.write({'discharge_medication_ids': [(5, 0, 0)]})
             for line in self.discharge_medication_ids:
                 discharge_medication_ids.append((0, 0, {
                     'product_id': line.product_id.id,
