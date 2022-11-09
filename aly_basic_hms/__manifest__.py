3d7d4701607585d93a10817f8031f719b84a037b
Aly Abdel Hamid <aly5elnemr@gmail.com>
2022-11-09 14:19:11 +0200

    [add] chatter5

--- aly_basic_hms/__manifest__.py
+++ aly_basic_hms/__manifest__.py
@@ -10,7 +10,7 @@
     "description": """
     'Aly El Nemr' upgraded a module developed by BrowseInfo, was deployed on Odoo 14 third-party apps.
 """,
-    "depends": ["base", "sale_management", "stock", "account", "mail"],
+    "depends": ["base", "sale_management", "stock", "account"],
     # "depends": ["base", "sale_management", "stock", "account_accountant"],
     "data": [
         'security/hospital_groups.xml',
