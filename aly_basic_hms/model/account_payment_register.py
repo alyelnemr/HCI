cf568624d301520d61125d2375d34aa7f2d3dcb5
Aly El Nemr <aly5elnemr@gmail.com>
2023-04-25 18:13:45 +0300

    [fix] issue in bank fees currency

--- aly_basic_hms/model/account_payment_register.py
+++ aly_basic_hms/model/account_payment_register.py
@@ -8,19 +8,32 @@ from odoo.exceptions import ValidationError
 class AccountPaymentRegister(models.TransientModel):
     _inherit = 'account.payment.register'
 
-    @api.onchange('amount', 'journal_id_type', 'journal_id', 'payment_date')
+    @api.onchange('amount', 'journal_id_type', 'journal_id_select', 'payment_date')
     def _compute_bank_fees(self):
         self.bank_fees_amount = 0
         self.is_bank_fees = False
         self.total_amount_with_fees = self.amount
-        if self.amount and self.journal_id_type == 'bank' and self.env.company.aly_enable_bank_fees:
+        self.journal_id = self.env['account.journal'].search([
+                    ('type', '=', 'cash'),
+                    ('company_id', '=', self.company_id.id),
+                ], limit=1)
+        if self.amount and self.journal_id_select == 'bank' and self.env.company.aly_enable_bank_fees:
             self.is_bank_fees = True
             self.bank_fees_amount = self.amount * self.env.company.aly_bank_fees_percentage
             self.total_amount_with_fees = self.amount + self.bank_fees_amount
+            self.journal_id = self.env['account.journal'].search([
+                ('type', '=', 'bank'),
+                ('company_id', '=', self.company_id.id),
+            ], limit=1)
 
     is_bank_fees = fields.Boolean(default=False)
     bank_fees_amount = fields.Monetary(string="Bank Fees", compute='_compute_bank_fees', store=False)
     total_amount_with_fees = fields.Monetary(string="Total Amount with Fees", compute='_compute_bank_fees', store=False)
+    journal_id_select = fields.Selection([
+        ('cash', 'Cash'),
+        ('bank', 'Bank'),
+    ], string="Journal", default='cash')
+    journal_id = fields.Many2one('account.journal', store=True, readonly=False)
     journal_id_type = fields.Selection([
         ('sale', 'Sales'),
         ('purchase', 'Purchase'),
@@ -47,4 +60,4 @@ class AccountPaymentRegister(models.TransientModel):
                 raise ValidationError(_("You are not allowed to select a negative value"))
             if rec.bank_fees_amount > 0 and not rec.env.company.aly_bank_fees_account:
                 raise ValidationError(_("Please set bank charge account in company screen."))
-        return super(AccountPaymentRegister, self).action_create_payments()
+        return super(AccountPaymentRegister, self).sudo().action_create_payments()
