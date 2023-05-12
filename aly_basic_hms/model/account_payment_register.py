2fa8d328a8b22156f9f5edbab4c67112ed330973
Aly El Nemr <aly5elnemr@gmail.com>
2023-05-12 19:28:21 +0300

    [remove] remove bank fees feature

--- aly_basic_hms/model/account_payment_register.py
+++ aly_basic_hms/model/account_payment_register.py
@@ -45,6 +45,82 @@ class AccountPaymentRegister(models.TransientModel):
     ], related="journal_id.type")
     is_insurance_patient = fields.Boolean(default=False)
 
+    def _post_payments(self, to_process, edit_mode=False):
+        """ Post the newly created payments.
+
+        :param to_process:  A list of python dictionary, one for each payment to create, containing:
+                            * create_vals:  The values used for the 'create' method.
+                            * to_reconcile: The journal items to perform the reconciliation.
+                            * batch:        A python dict containing everything you want about the source journal items
+                                            to which a payment will be created (see '_get_batches').
+        :param edit_mode:   Is the wizard in edition mode.
+        """
+        payments = self.env['account.payment']
+        for vals in to_process:
+            payments |= vals['payment']
+        payments.sudo().action_post()
+
+    def _reconcile_payments(self, to_process, edit_mode=False):
+        """ Reconcile the payments.
+
+        :param to_process:  A list of python dictionary, one for each payment to create, containing:
+                            * create_vals:  The values used for the 'create' method.
+                            * to_reconcile: The journal items to perform the reconciliation.
+                            * batch:        A python dict containing everything you want about the source journal items
+                                            to which a payment will be created (see '_get_batches').
+        :param edit_mode:   Is the wizard in edition mode.
+        """
+        domain = [
+            ('parent_state', '=', 'posted'),
+            ('account_internal_type', 'in', ('receivable', 'payable')),
+            ('reconciled', '=', False),
+        ]
+        for vals in to_process:
+            payment_lines = vals['payment'].line_ids.filtered_domain(domain)
+            lines = vals['to_reconcile']
+
+            for account in payment_lines.account_id:
+                (payment_lines + lines)\
+                    .filtered_domain([('account_id', '=', account.id), ('reconciled', '=', False)])\
+                    .sudo().reconcile()
+
+    def _create_payments(self):
+        self.ensure_one()
+        batches = self._get_batches()
+        edit_mode = self.can_edit_wizard and (len(batches[0]['lines']) == 1 or self.group_payment)
+        to_process = []
+
+        if edit_mode:
+            payment_vals = self._create_payment_vals_from_wizard()
+            to_process.append({
+                'create_vals': payment_vals,
+                'to_reconcile': batches[0]['lines'],
+                'batch': batches[0],
+            })
+        else:
+            # Don't group payments: Create one batch per move.
+            if not self.group_payment:
+                new_batches = []
+                for batch_result in batches:
+                    for line in batch_result['lines']:
+                        new_batches.append({
+                            **batch_result,
+                            'lines': line,
+                        })
+                batches = new_batches
+
+            for batch_result in batches:
+                to_process.append({
+                    'create_vals': self._create_payment_vals_from_batch(batch_result),
+                    'to_reconcile': batch_result['lines'],
+                    'batch': batch_result,
+                })
+
+        payments = self._init_payments(to_process, edit_mode=edit_mode)
+        self._post_payments(to_process, edit_mode=edit_mode)
+        self._reconcile_payments(to_process, edit_mode=edit_mode)
+        return payments
+
     # def _create_payment_vals_from_wizard(self):
     #     res = super(AccountPaymentRegister, self)._create_payment_vals_from_wizard()
     #     res['bank_fees_amount'] = self.bank_fees_amount
@@ -57,34 +133,34 @@ class AccountPaymentRegister(models.TransientModel):
     #     res['is_bank_fees'] = self.is_bank_fees
     #     return res
     #
-    # def action_create_payments(self):
-    #     for rec in self:
-    #         if rec.bank_fees_amount < 0:
-    #             raise ValidationError(_("You are not allowed to select a negative value"))
-    #         if rec.bank_fees_amount > 0 and not rec.env.company.aly_bank_fees_account:
-    #             raise ValidationError(_("Please set bank charge account in company screen."))
-    #     payments = self.with_user(SUPERUSER_ID)._create_payments()
-    #
-    #     if self._context.get('dont_redirect_to_payments'):
-    #         return True
-    #
-    #     action = {
-    #         'name': _('Payments'),
-    #         'type': 'ir.actions.act_window',
-    #         'res_model': 'account.payment',
-    #         'context': {'create': False},
-    #     }
-    #     if len(payments) == 1:
-    #         action.update({
-    #             'view_mode': 'form',
-    #             'res_id': payments.id,
-    #         })
-    #     else:
-    #         action.update({
-    #             'view_mode': 'tree,form',
-    #             'domain': [('id', 'in', payments.ids)],
-    #         })
-    #     return action
+    def action_create_payments(self):
+        for rec in self:
+            if rec.bank_fees_amount < 0:
+                raise ValidationError(_("You are not allowed to select a negative value"))
+            if rec.bank_fees_amount > 0 and not rec.env.company.aly_bank_fees_account:
+                raise ValidationError(_("Please set bank charge account in company screen."))
+        payments = self.with_user(SUPERUSER_ID)._create_payments()
+
+        if self._context.get('dont_redirect_to_payments'):
+            return True
+
+        action = {
+            'name': _('Payments'),
+            'type': 'ir.actions.act_window',
+            'res_model': 'account.payment',
+            'context': {'create': False},
+        }
+        if len(payments) == 1:
+            action.update({
+                'view_mode': 'form',
+                'res_id': payments.id,
+            })
+        else:
+            action.update({
+                'view_mode': 'tree,form',
+                'domain': [('id', 'in', payments.ids)],
+            })
+        return action
 
     # @api.model
     # def default_get(self, fields_list):
