d816dc2091df67c48dc146b59ca65c65e1722d4e
Aly El Nemr <aly5elnemr@gmail.com>
2023-10-09 14:27:28 +0300

    [fix] issue in service charge

--- aly_basic_hms/model/MedicalInvoiceTemplate.py
+++ aly_basic_hms/model/MedicalInvoiceTemplate.py
@@ -99,11 +99,11 @@ class MedicalInvoiceTemplate(models.AbstractModel):
         var_subtotal = docs.amount_untaxed - (var_disposable + var_prosthetics + var_medicine + var_service_charge) if (docs.amount_untaxed - (var_disposable + var_prosthetics + var_medicine)) >= 1 else 0
         var_service_charge = sale_order.service_charge_amount
         aly_service_charge_percentage = float(sale_order.company_id.aly_service_charge_percentage)
-        var_service_untaxed_amount = sale_order.service_untaxed_amount
+        # var_service_untaxed_amount = sale_order.service_untaxed_amount
         # var_subtotal = var_subtotal - var_service_charge
         var_discount = round(var_subtotal_with_discount - docs.amount_untaxed, 2)
         var_subtotal += var_discount
-        var_amount_total = var_subtotal + var_discount + var_bank_fees_amount + var_service_charge + (var_disposable + var_prosthetics + var_medicine + var_service_charge)
+        var_amount_total = var_subtotal + var_bank_fees_amount + (var_disposable + var_prosthetics + var_medicine + var_service_charge)
         # var_amount_total_taxed = var_subtotal + var_taxed_amount + var_disposable + var_prosthetics + var_medicine + var_bank_fees_amount
         var_amount_total_taxed = var_subtotal + var_taxed_amount
         discount_total = sale_order.discount_total
@@ -127,7 +127,7 @@ class MedicalInvoiceTemplate(models.AbstractModel):
             'var_service_charge': var_service_charge,
             'var_bank_fees_amount': var_bank_fees_amount,
             'aly_service_charge_percentage': aly_service_charge_percentage,
-            'var_service_untaxed_amount': var_service_untaxed_amount,
+            # 'var_service_untaxed_amount': var_service_untaxed_amount,
             'var_amount_total_taxed': var_amount_total_taxed,
             'var_discount': var_discount,
             'var_subtotal_with_discount': var_subtotal_with_discount,
