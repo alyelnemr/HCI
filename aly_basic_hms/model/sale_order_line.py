142f119a81aa804dcd83c83c6ac5693dba4bdd12
Aly El Nemr <aly5elnemr@gmail.com>
2023-11-10 01:24:34 +0300

    [fix] issue in service charge

--- aly_basic_hms/model/sale_order_line.py
+++ aly_basic_hms/model/sale_order_line.py
@@ -1,8 +1,5 @@
 from odoo import api, fields, models, _
-from odoo.exceptions import UserError, ValidationError, AccessError
-from odoo.tools.misc import formatLang, format_date, get_lang
-from odoo.tools import float_compare, date_utils, email_split, email_re, float_is_zero
-from collections import defaultdict
+from odoo.exceptions import UserError
 
 
 class SaleOrderLine(models.Model):
