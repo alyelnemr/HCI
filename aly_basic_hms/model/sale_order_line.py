d2015c44d32bb9587da0b60c4a3abe4c98d61f0b
a.abdelhamid <a.abdelhamid@fayendra.com>
2024-02-20 15:31:03 +0300

    [fix] issue in payment method of external services

--- aly_basic_hms/model/sale_order_line.py
+++ aly_basic_hms/model/sale_order_line.py
@@ -7,6 +7,8 @@ class SaleOrderLine(models.Model):
 
     def unlink(self):
         for rec in self:
-            if rec.product_id == self.company_id.aly_service_product_id:
-                raise UserError('You cannot remove the service charge.')
+            has_my_group = self.env.user.has_group('aly_basic_hms.aly_group_medical_manager')
+            if not has_my_group and not self.env.su:
+                if rec.product_id == self.company_id.aly_service_product_id:
+                    raise UserError('You cannot remove the service charge.')
         return super(SaleOrderLine, self).unlink()
