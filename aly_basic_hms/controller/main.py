8b3d4fad09e6cac71b848dbd7eea2721c7c9542d
a.abdelhamid <a.abdelhamid@fayendra.com>
2024-03-20 00:54:57 +0300

    [fix] issue in patients permission

--- aly_basic_hms/controller/main.py
+++ aly_basic_hms/controller/main.py
@@ -22,13 +22,14 @@ class Binary(http.Controller):
         try:
             return base64.decodestring(s)
         except Exception as e:
-            raise ValidationError('Error:', +str(e))
+            raise ValidationError('Error: ' + str(e))
+
     @http.route('/web/binary/download_document', type='http', auth="public")
     def download_document(self, tab_id, **kw):
         new_tab = ast.literal_eval(tab_id)
         patient_id = request.env['medical.patient'].search([('id', 'in', new_tab)], limit=1)
         attachment_ids = request.env['medical.patient.attachment'].search([('patient_id', 'in', new_tab)])
-        zip_filename = patient_id.name # datetime.now()
+        zip_filename = patient_id.name  # datetime.now()
         zip_filename = "%s.zip" % zip_filename
         bitIO = BytesIO()
         zip_file = zipfile.ZipFile(bitIO, "w", zipfile.ZIP_DEFLATED)
@@ -43,4 +44,3 @@ class Binary(http.Controller):
         return request.make_response(bitIO.getvalue(),
                                      headers=[('Content-Type', 'application/x-zip-compressed'),
                                               ('Content-Disposition', content_disposition(zip_filename))])
-
